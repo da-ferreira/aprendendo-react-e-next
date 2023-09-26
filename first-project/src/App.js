8e78cf9cad096bd80a19d5b4be650ef993a75ddb
David Ferreira <ferreiradavid361@gmail.com>
2023-09-26 15:18:39 -0300

    props e organização de componentes: partes 1 e 2

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,40 +1,27 @@
 import './App.css';
 import { Component } from 'react';
+import { loadPosts } from './utils/loadPosts';
+import { Posts } from './components/Posts';
 
 // 👉 Componente de classe
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1',
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2',
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3',
-      },
-    ],
+    posts: [],
   };
+
+  async componentDidMount() {
+    const postsAndPhotos = await loadPosts();
+
+    this.setState({ posts: postsAndPhotos });
+  }
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1 key={post.id}>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
     );
   }
 }
