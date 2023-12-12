daa53f42ef84b9496b45bfaeca573bf83c606d0b
David Ferreira <ferreiradavid361@gmail.com>
2023-12-11 21:25:42 -0300

    roteamento no react: continuacao

---
+++ tutorial-react-router-dom/src/components/Menu/index.tsx
@@ -0,0 +1,23 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/posts/10">Posts - 10</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
-import { Home } from './components/Home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import { Home } from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About } from './components/About';
+import { Menu } from './components/Menu';
+import { Post } from './components/Post';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
