ab99d1b696a70c5775a00fe3ad2db83716a18cda
David Ferreira <ferreiradavid361@gmail.com>
2023-12-13 10:25:03 -0300

    react router dom: continuação

--- tutorial-react-router-dom/src/components/Post/index.tsx
+++ tutorial-react-router-dom/src/components/Post/index.tsx
@@ -1,9 +1,17 @@
+import { Outlet, useParams, useSearchParams } from 'react-router-dom'
 import './style.css'
 
 export const Post = () => {
+  const { id } = useParams()
+  const [qs] = useSearchParams()
+
+  console.log('qs', qs)
+
   return (
     <div>
-      <h1>Post</h1>
+      <h1>Post - {id} - {`qs: ${qs}`}</h1>
+
+      <Outlet />
     </div>
   )
 }
--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { About } from './components/About';
 import { Menu } from './components/Menu';
 import { Post } from './components/Post';
+import { NotFound } from './components/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -15,7 +16,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Post />} />
+        <Route path="/posts" element={<Post />}>
+          <Route path=":id" element={<div>Oi</div>} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
